1e102ad1e9c69af75119b5aa6b587d5a72ec85a7
Pedro Lopes <plopes@rocket-internet.pt>
2014-09-05 17:57:37 +0100

    % Fix image size and placeholder images

---
+++ Jumia/Categories/UIImageView+JA.h
@@ -0,0 +1,25 @@
+//
+//  UIImageView+JA.h
+//  Jumia
+//
+//  Created by Pedro Lopes on 05/09/14.
+//  Copyright (c) 2014 Rocket Internet. All rights reserved.
+//
+
+@interface UIImageView (JA)
+
+/**
+ Method to set the image
+ @param image The image to be setted
+ @param height The height of the image to be setted
+ @param width The width of the image to be setted
+ 
+ */
+- (void) setImage:(UIImage*)image
+       withHeight:(CGFloat)height
+         andWidth:(CGFloat)width;
+
+- (void) changeImageSize:(CGFloat)height
+                andWidth:(CGFloat)width;
+
+@end
