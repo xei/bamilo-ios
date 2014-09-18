4bfa664afb881e1f80c55de2115ee35ab9beec7c
Pedro Lopes <plopes@rocket-internet.pt>
2014-09-18 12:16:22 +0100

    % NAFAMZ-8123: Add adjust sdk to application

---
+++ Jumia/RICommunicationFramework/Wrappers/RITrackingWrapper/Trackers/RIAdjustTracker.h
@@ -0,0 +1,18 @@
+//
+//  RIAdjustTracker.h
+//  Jumia
+//
+//  Created by Pedro Lopes on 18/09/14.
+//  Copyright (c) 2014 Rocket Internet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const kRIAdjustToken;
+
+@interface RIAdjustTracker : NSObject
+<
+RITracker
+>
+
+@end
