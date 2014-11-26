ad1a1f4fb9b475782fa7ad80c34f2aa5cad71aa5
tpinto-rocket <tpinto@rocket-internet.pt>
2014-11-26 17:40:06 +0000

    Fixed recent label in grid catalog

--- Jumia/View/Cells/Catalog/JACatalogGridCell.m
+++ Jumia/View/Cells/Catalog/JACatalogGridCell.m
@@ -26,6 +26,16 @@
                                       181.0f,
                                       self.frame.size.width - 4.0f,
                                       self.priceView.frame.size.height);
+    
+    [self.recentLabel removeFromSuperview];
+    self.recentLabel = [[UILabel alloc] initWithFrame:CGRectMake(-2.0f, 17.0f, 48.0f, 14.0f)];
+    self.recentLabel.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:8.0f];
+    self.recentLabel.text = STRING_NEW;
+    self.recentLabel.textAlignment = NSTextAlignmentCenter;
+    self.recentLabel.textColor = [UIColor whiteColor];
+    self.recentLabel.transform = CGAffineTransformMakeRotation (-M_PI/4);
+    [self addSubview:self.recentLabel];
+    self.recentLabel.hidden = ![product.isNew boolValue];
 }
 
 @end
