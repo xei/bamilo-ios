fe84fa99798a25338c6209c14529ec22508221ca
tpinto-rocket <tpinto@rocket-internet.pt>
2014-12-11 17:47:34 +0000

    Custom page control implemented and being used in gallery view

---
+++ Jumia/View/PDVComponents/JAPageControl/JAPageControl.h
@@ -0,0 +1,13 @@
+//
+//  JAPageControl.h
+//  Jumia
+//
+//  Created by Telmo Pinto on 11/12/14.
+//  Copyright (c) 2014 Rocket Internet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JAPageControl : UIPageControl
+
+@end
