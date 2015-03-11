c3994c55ce4e1b7596aee728168ecf06a60d4244
epacheco <epacheco@rocket-internet.pt>
2015-03-11 20:30:18 +0000

    Reviews Scroll fixed, updated

--- Jumia/Controllers/Ratings/JARatingsViewController.m
+++ Jumia/Controllers/Ratings/JARatingsViewController.m
@@ -93,11 +93,6 @@ UITableViewDataSource
     }
     
     [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(keyboardWillShow:)
-                                                 name:UIKeyboardWillShowNotification
-                                               object:nil];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(keyboardWillHide:)
                                                  name:UIKeyboardWillHideNotification
                                                object:nil];
