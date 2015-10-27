f1970b57214a90233145442550b347e8fb9710ae
tpinto-rocket <tpinto@rocket-internet.pt>
2015-10-27 11:08:48 +0000

    Tab bar labels

--- Jumia/View/GlobalTabBar/JATabBarView.m
+++ Jumia/View/GlobalTabBar/JATabBarView.m
@@ -10,6 +10,7 @@
 #import "JATabBarButton.h"
 
 #define kButtonIds @[@"home", @"wishlist", @"cart", @"user", @"more"]
+#define kButtonStrings @[STRING_HOME, STRING_MY_FAVOURITES, STRING_CART, STRING_MY_ACCOUNT, STRING_MORE]
 
 @interface JATabBarView()
 
@@ -29,7 +30,8 @@
     CGFloat currentX = 0.0f;
     for (int i = 0; i<kButtonIds.count; i++) {
         NSString* identifier = [kButtonIds objectAtIndex:i];
-
+        NSString* string = [kButtonStrings objectAtIndex:i];
+        
         CGFloat width = self.frame.size.width/kButtonIds.count;
         
         NSString* buttonNameNormal = [NSString stringWithFormat:@"tabbar_button_%@", identifier];
@@ -41,7 +43,7 @@
                                      width,
                                      self.bounds.size.height);
         tabButton.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
-        [tabButton setupWithImageName:buttonNameNormal highlightedImageName:buttonNameHighlighted title:identifier];
+        [tabButton setupWithImageName:buttonNameNormal highlightedImageName:buttonNameHighlighted title:string];
         tabButton.clickableView.tag = i;
         [tabButton.clickableView addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:tabButton];
