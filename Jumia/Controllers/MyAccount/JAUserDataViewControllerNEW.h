5c995b173cb46d5b0b2ef83fd73343d4fa9ed47a
LDaniel Duarte <luciano.duarte@africainternetgroup.com>
2015-12-21 15:36:55 +0000

    NAFAMZ-14284: Edit user form elements

---
+++ Jumia/Controllers/MyAccount/JAUserDataViewControllerNEW.h
@@ -0,0 +1,13 @@
+//
+//  JAUserDataViewControllerNEW.h
+//  Jumia
+//
+//  Created by lucianoduarte on 16/12/15.
+//  Copyright © 2015 Rocket Internet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JAUserDataViewControllerNEW : JABaseViewController
+
+@end
