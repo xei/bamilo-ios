b9faf155a8b6381cfd73ec37e839b33bc8796080
Jose Mota <jose.mota@africainternetgroup.com>
2016-02-25 16:32:59 +0000

    fixing translate script (adding translation files only to commit)

--- Jumia/Scripts/web_translate_it.sh
+++ Jumia/Scripts/web_translate_it.sh
@@ -15,7 +15,13 @@ git config --local user.email "mobile-rocket-porto@gmail.com"
 git config --local user.name "mobile-rocket-porto"
 git config --local credential.helper 'store --file=credentials.git' 
 
-git add .
+git add Jumia/ar.lproj/RIStrings.strings
+git add Jumia/fa.lproj/RIStrings.strings
+git add Jumia/fr.lproj/RIStrings.strings
+git add Jumia/my.lproj/RIStrings.strings
+git add Jumia/pt.lproj/RIStrings.strings
+git add Jumia/ur.lproj/RIStrings.strings
+
 git commit -m "Uploading new translations from wti"
 git push origin HEAD:development 
 
