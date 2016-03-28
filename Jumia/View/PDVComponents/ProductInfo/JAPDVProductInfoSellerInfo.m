ada8bcc3e4e7c077fe4308975e0bec5f5f13520f
Jose Mota <jose.mota@africainternetgroup.com>
2016-03-28 19:04:03 +0100

    NAFAMZ-16743 - Update "Seller info" section on PDV to match latest style guide and component updates

--- Jumia/View/PDVComponents/ProductInfo/JAPDVProductInfoSellerInfo.m
+++ Jumia/View/PDVComponents/ProductInfo/JAPDVProductInfoSellerInfo.m
@@ -8,6 +8,7 @@
 
 #import "JAPDVProductInfoSellerInfo.h"
 #import "JAProductInfoRatingLine.h"
+#import "JAProductInfoBaseLine.h"
 #import "JAClickableView.h"
 
 @interface JAPDVProductInfoSellerInfo ()
@@ -20,7 +21,7 @@
 @property (nonatomic) UILabel *sellerWarrantyLabel;
 @property (nonatomic) UIImageView *shippingIcon;
 @property (nonatomic) UIImageView *warrantyIcon;
-@property (nonatomic) UIButton* linkGlobalButton;
+@property (nonatomic) JAProductInfoBaseLine* linkGlobalButton;
 @property (nonatomic) JAClickableView *clickableView;
 
 @end
@@ -38,13 +39,13 @@
 
 - (UIImageView *)arrow
 {
-    CGRect frame = CGRectMake(self.width - 8 - 16, self.height/2 - 6, 8, 12);
+    CGRect frame = CGRectMake(self.width - 8 - 16, self.clickableView.height/2 - 6, 8, 12);
     if (!VALID_NOTEMPTY(_arrow, UIImageView)) {
         _arrow = [[UIImageView alloc] initWithFrame:frame];
         [_arrow setImage:[UIImage imageNamed:@"arrow_moreinfo"]];
         [_arrow setHidden:YES];
         [_arrow setX:self.width - 16 - _arrow.width];
-        [_arrow setY:self.height/2 - _arrow.height/2];
+        [_arrow setY:self.clickableView.height/2 - _arrow.height/2];
         if (RI_IS_RTL) {
             [_arrow flipViewImage];
         }
@@ -59,8 +60,8 @@
 {
     if (!VALID_NOTEMPTY(_sellerNameLabel, UILabel)) {
         _sellerNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, 16, self.width-32, 20)];
-        [_sellerNameLabel setFont:JABodyFont];
-        [_sellerNameLabel setTextColor:JABlack900Color];
+        [_sellerNameLabel setFont:JATitleFont];
+        [_sellerNameLabel setTextColor:JABlackColor];
         [self.clickableView addSubview:_sellerNameLabel];
     }
     return _sellerNameLabel;
@@ -71,7 +72,7 @@
     if (!VALID_NOTEMPTY(_sellerDeliveryLabel, UILabel)) {
         _sellerDeliveryLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, CGRectGetMaxY(self.sellerNameLabel.frame) + 8.f, self.width-32, 20)];
         [_sellerDeliveryLabel setFont:JABodyFont];
-        [_sellerDeliveryLabel setTextColor:JABlack800Color];
+        [_sellerDeliveryLabel setTextColor:JABlackColor];
         [self.clickableView addSubview:_sellerDeliveryLabel];
     }
     return _sellerDeliveryLabel;
@@ -82,8 +83,8 @@
     if (!VALID_NOTEMPTY(_sellerDeliveryTimeLabel, UILabel)) {
         _sellerDeliveryTimeLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, CGRectGetMaxY(self.sellerDeliveryLabel.frame) + 8.f, self.width-32, 20)];
         _sellerDeliveryTimeLabel.numberOfLines = 0;
-        [_sellerDeliveryTimeLabel setFont:JACaptionFont];
-        [_sellerDeliveryTimeLabel setTextColor:JABlack800Color];
+        [_sellerDeliveryTimeLabel setFont:JABodyFont];
+        [_sellerDeliveryTimeLabel setTextColor:JABlackColor];
         [_sellerDeliveryTimeLabel setTextAlignment:NSTextAlignmentLeft];
         [self.clickableView addSubview:_sellerDeliveryTimeLabel];
     }
@@ -93,9 +94,9 @@
 - (UILabel *)shippingGlobalLabel
 {
     if (!VALID_NOTEMPTY(_shippingGlobalLabel, UILabel)) {
-        _shippingGlobalLabel = [[UILabel alloc] initWithFrame:CGRectMake(48, CGRectGetMaxY(self.sellerDeliveryLabel.frame) + 8.f, self.width-32, 20)];
+        _shippingGlobalLabel = [[UILabel alloc] initWithFrame:CGRectMake(48, CGRectGetMaxY(self.sellerDeliveryLabel.frame) + 8.f, self.width-(self.width-self.arrow.x)-48, 20)];
         [_shippingGlobalLabel setFont:JACaptionFont];
-        [_shippingGlobalLabel setTextColor:JAOrange1Color];
+        [_shippingGlobalLabel setTextColor:JABlack800Color];
         _shippingGlobalLabel.numberOfLines = 0;
         [_shippingGlobalLabel setTextAlignment:NSTextAlignmentLeft];
         [_shippingGlobalLabel setHidden:YES];
@@ -121,13 +122,14 @@
     return _shippingIcon;
 }
 
-- (UIButton *)linkGlobalButton
+- (JAProductInfoBaseLine *)linkGlobalButton
 {
-    if (!VALID_NOTEMPTY(_linkGlobalButton, UIButton)) {
-        _linkGlobalButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [_linkGlobalButton setFrame:CGRectMake(48, CGRectGetMaxY(self.shippingIcon.frame) + 16.f, self.width-32, 20)];
-        [_linkGlobalButton.titleLabel setFont:JACaptionFont];
-        [_linkGlobalButton.titleLabel setTextColor:JAOrange1Color];
+    if (!VALID_NOTEMPTY(_linkGlobalButton, JAProductInfoBaseLine)) {
+        _linkGlobalButton = [[JAProductInfoBaseLine alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.shippingIcon.frame) + 16.f, self.width, kProductInfoSingleLineHeight)];
+        [_linkGlobalButton setTopSeparatorXOffset:16.f];
+        [_linkGlobalButton setTopSeparatorVisibility:YES];
+        [_linkGlobalButton.titleLabel setFont:JABodyFont];
+        [_linkGlobalButton.titleLabel setTextColor:JABlue1Color];
         [_linkGlobalButton setHidden:YES];
         [self addSubview:_linkGlobalButton];
     }
@@ -140,8 +142,8 @@
         _warrantyIcon = [UIImageView new];
         [_warrantyIcon setImage:[UIImage imageNamed:@"warranty"]];
         [_warrantyIcon sizeToFit];
-        [_warrantyIcon setX:self.sellerDeliveryLabel.x];
-        [_warrantyIcon setY:CGRectGetMaxY(self.sellerDeliveryLabel.frame)+10.f];
+        [_warrantyIcon setX:16.f];
+        [_warrantyIcon setY:CGRectGetMaxY(self.sellerNameLabel.frame)+16.f];
         if (RI_IS_RTL) {
             [_warrantyIcon flipViewImage];
         }
@@ -154,10 +156,11 @@
 - (UILabel *)sellerWarrantyLabel
 {
     if (!VALID_NOTEMPTY(_sellerWarrantyLabel, UILabel)) {
-        _sellerWarrantyLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, CGRectGetMaxY(self.sellerDeliveryLabel.frame) + 8.f, self.width-32, 20)];
+        _sellerWarrantyLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, CGRectGetMaxY(self.sellerDeliveryLabel.frame) + 16.f, self.width-32, 20)];
         [_sellerWarrantyLabel setNumberOfLines:2];
         [_sellerWarrantyLabel setFont:JABodyFont];
-        [_sellerWarrantyLabel setTextColor:JABlack800Color];
+        [_sellerWarrantyLabel setTextColor:JABlackColor];
+        [_sellerWarrantyLabel setTextAlignment:NSTextAlignmentLeft];
         [self.clickableView addSubview:_sellerWarrantyLabel];
     }
     return _sellerWarrantyLabel;
@@ -168,7 +171,7 @@
     if (!_shopFirstLogo) {
         _shopFirstLogo = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"shop_first_logo"]];
         [_shopFirstLogo sizeToFit];
-        [_shopFirstLogo setX:CGRectGetMaxX(self.sellerNameLabel.frame) + 8.f];
+        [_shopFirstLogo setX:CGRectGetMaxX(self.sellerNameLabel.frame) + 10.f];
         [_shopFirstLogo setY:self.sellerNameLabel.y];
         [_shopFirstLogo setUserInteractionEnabled:YES];
         UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self
@@ -188,16 +191,35 @@
     
     [self checkIsShopFirst];
     
-    [self.sellerDeliveryLabel setText:seller.deliveryTime];
-    [self.sellerDeliveryLabel setFont:JACaptionFont];
-    [self.sellerDeliveryLabel setTextColor:JABlackColor];
-    [self.sellerDeliveryLabel sizeToFit];
+    [self setHeight:CGRectGetMaxY(self.sellerNameLabel.frame) + 16.f];
+    [self.clickableView setHeight:CGRectGetMaxY(self.sellerNameLabel.frame) + 16.f];
+    [self.sellerDeliveryLabel setY:CGRectGetMaxY(self.sellerNameLabel.frame) + 16.f];
+    
+    if (VALID_NOTEMPTY(seller.warranty, NSString)) {
+        [self.sellerWarrantyLabel setText:[NSString stringWithFormat:@"%@: %@", STRING_SELLER_INFO_WARRANTY, [seller.warranty uppercaseString]]];
+        
+        [self.sellerWarrantyLabel setFrame:CGRectMake(CGRectGetMaxX(self.warrantyIcon.frame)+10.f, CGRectGetMaxY(self.sellerNameLabel.frame) + 16.f, self.width - (CGRectGetMaxX(self.warrantyIcon.frame) + 10 + (self.width - self.arrow.x)), [self.sellerWarrantyLabel sizeThatFits:CGSizeMake(self.sellerWarrantyLabel.width, CGFLOAT_MAX)].height)];
+        
+        [self.warrantyIcon setY:CGRectGetMidY(self.sellerWarrantyLabel.frame) - self.warrantyIcon.height/2];
+        [self.warrantyIcon setHidden:NO];
+        [self setHeight:CGRectGetMaxY(self.sellerWarrantyLabel.frame) + 16.f];
+        [self.clickableView setHeight:CGRectGetMaxY(self.sellerWarrantyLabel.frame) + 16.f];
+        [self.sellerDeliveryLabel setY:CGRectGetMaxY(self.sellerWarrantyLabel.frame) + 16.f];
+    }
+    
+    if (VALID_NOTEMPTY(seller.deliveryTime, NSString)) {
+        
+        [self.sellerDeliveryLabel setText:[seller.deliveryTime uppercaseString]];
+        [self.sellerDeliveryLabel sizeToFit];
+        [self setHeight:CGRectGetMaxY(self.sellerDeliveryLabel.frame) + 16.f];
+        [self.clickableView setHeight:CGRectGetMaxY(self.sellerDeliveryLabel.frame) + 16.f];
+        [self.sellerDeliveryTimeLabel setY:CGRectGetMaxY(self.sellerDeliveryLabel.frame) + 16.f];
+    }
     
     if (seller.isGlobal) {
         [self.sellerDeliveryLabel setX:self.shippingGlobalLabel.x];
 
-        [self.sellerDeliveryTimeLabel setText:seller.cmsInfo];
-        [self.sellerDeliveryTimeLabel setTextColor:JABlackColor];
+        [self.sellerDeliveryTimeLabel setText:[seller.cmsInfo uppercaseString]];
         [self.sellerDeliveryTimeLabel setX:self.shippingGlobalLabel.x];
         [self.sellerDeliveryTimeLabel setWidth:self.width - self.sellerDeliveryTimeLabel.frame.origin.x - 18.f];
         [self.sellerDeliveryTimeLabel setY:CGRectGetMaxY(self.sellerDeliveryLabel.frame)+.4f];
@@ -205,41 +227,20 @@
 
         [self.shippingGlobalLabel setHidden:NO];
         [self.shippingGlobalLabel setText:[seller.shippingGlobal stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\r"]]];
-        [self.shippingGlobalLabel sizeToFit];
+        [self.shippingGlobalLabel setHeight:[self.shippingGlobalLabel sizeThatFits:CGSizeMake(self.shippingGlobalLabel.width, CGFLOAT_MAX)].height];
         [self.shippingGlobalLabel setY:CGRectGetMaxY(self.sellerDeliveryTimeLabel.frame)+.4f];
-
+        
+        [self.shippingIcon setX:CGRectGetMinX(self.warrantyIcon.frame)];
         [self.shippingIcon setHidden:NO];
-        [self.shippingIcon setY:self.sellerDeliveryLabel.y];
+        [self.shippingIcon setY:CGRectGetMidY(self.sellerDeliveryLabel.frame)-self.shippingIcon.height/2];
         
         [self.linkGlobalButton setHidden:NO];
-        NSDictionary *underlineAttribute = @{NSUnderlineStyleAttributeName: @(NSUnderlineStyleSingle)};
-        [self.linkGlobalButton setAttributedTitle:[[NSAttributedString alloc] initWithString:seller.linkTextGlobal
-                                                                            attributes:underlineAttribute]
-                                   forState:UIControlStateNormal];
-        [self.linkGlobalButton.titleLabel sizeToFit];
-        [self.linkGlobalButton setY:CGRectGetMaxY(self.shippingGlobalLabel.frame)+8.f];
-        [self.linkGlobalButton sizeToFit];
+        [self.linkGlobalButton setTitle:seller.linkTextGlobal];
+        [self.linkGlobalButton setY:CGRectGetMaxY(self.shippingGlobalLabel.frame)+16.f];
         
-        [self.warrantyIcon setX:CGRectGetMinX(self.shippingIcon.frame)];
-        [self.warrantyIcon setY:CGRectGetMaxY(self.linkGlobalButton.frame) + 5.f];
+        [self setHeight:CGRectGetMaxY(self.linkGlobalButton.frame)];
+        [self.clickableView setHeight:CGRectGetMaxY(self.shippingGlobalLabel.frame) + 16.f];
     }
-    
-    if (!seller.isGlobal) {
-        [self setHeight:CGRectGetMaxY(self.sellerDeliveryLabel.frame) + 16.f];
-    } else {
-        [self setHeight:CGRectGetMaxY(self.linkGlobalButton.frame) + 16.f];
-    }
-    if (seller.warranty != NULL) {
-        [self.sellerWarrantyLabel setText:[NSString stringWithFormat:@"%@: %@", STRING_SELLER_INFO_WARRANTY, seller.warranty]];
-        [self.sellerWarrantyLabel setFont:JACaptionFont];
-        [self.sellerWarrantyLabel setTextColor:JABlackColor];
-        [self.sellerWarrantyLabel setFrame:CGRectMake(CGRectGetMaxX(self.warrantyIcon.frame) + 12.f, CGRectGetMidY(self.warrantyIcon.frame) - 7.f, self.width - (CGRectGetMaxX(self.warrantyIcon.frame) + 12.f + 16.f), self.sellerWarrantyLabel.height)];
-        [self.sellerWarrantyLabel setHeight:[self.sellerWarrantyLabel sizeThatFits:CGSizeMake(self.sellerWarrantyLabel.width, CGFLOAT_MAX)].height];
-        [self.warrantyIcon setHidden:NO];
-        [self setHeight:CGRectGetMaxY(self.sellerWarrantyLabel.frame) + 16.f];
-    }
-
-    [self.clickableView setHeight:[self height]];
     
     [self arrow];
 }
