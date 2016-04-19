9bf48a9aa32912a95e137f703d65fe4edfc1e475
Jose Mota <jose.mota@africainternetgroup.com>
2016-04-19 11:37:36 +0100

    NAFAMZ-16782 - Category Menu:: Display "external links" items set-up in the "App category mgr"

--- Jumia/RICommunicationFramework/Configuration/RIConfiguration.m
+++ Jumia/RICommunicationFramework/Configuration/RIConfiguration.m
@@ -132,3 +132,5 @@ NSString *const RI_API_MULTISTEP_GET_FINISH = @"multistep/getstepfinish/";
 NSString *const RI_API_MULTISTEP_SUBMIT_FINISH = @"multistep/finish/";
 NSString *const RI_API_GET_FAQ_AND_TERMS = @"/main/getfaqandterms/";
 NSString *const RI_API_GET_PHONE_PREFIXES = @"/main/getphoneprefixes/";
+
+NSString *const RI_API_EXTERNAL_LINKS = @"main/getexternallinks/";
---
+++ Jumia/RICommunicationFramework/Model/CoreDataEntities/RIExternalCategory.m
@@ -0,0 +1,97 @@
+//
+//  RIExternalCategory.m
+//  Jumia
+//
+//  Created by Jose Mota on 18/04/16.
+//  Copyright © 2016 Rocket Internet. All rights reserved.
+//
+
+#import "RIExternalCategory.h"
+#import "RITarget.h"
+
+@implementation RIExternalCategory
+
+@dynamic label;
+@dynamic position;
+@dynamic targetString;
+@dynamic imageUrl;
+@dynamic level;
+@dynamic urlKey;
+@dynamic children;
+@dynamic parent;
+
++ (void)getExternalCategoryWithSuccessBlock:(void (^)(RIExternalCategory *externalCategory))successBlock
+                                            andFailureBlock:(void (^)(RIApiResponse apiResponse, NSArray *errorMessage))failureBlock;
+{
+    NSArray* databaseParentCategories = [[RIDataBaseWrapper sharedInstance] getEntryOfType:NSStringFromClass([RIExternalCategory class]) withPropertyName:@"parent" andPropertyValue:nil];
+    if (databaseParentCategories.count > 0) {
+        successBlock([databaseParentCategories firstObject]);
+    }else{
+        [self loadExternalCategoryWithSuccessBlock:successBlock andFailureBlock:failureBlock];
+    }
+}
+
++ (NSString *)loadExternalCategoryWithSuccessBlock:(void (^)(RIExternalCategory *externalCategory))successBlock
+                                  andFailureBlock:(void (^)(RIApiResponse apiResponse, NSArray *errorMessage))failureBlock
+{
+    return [[RICommunicationWrapper sharedInstance] sendRequestWithUrl:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@", [RIApi getCountryUrlInUse], RI_API_VERSION, RI_API_EXTERNAL_LINKS]]
+                                                            parameters:nil httpMethod:HttpResponsePost
+                                                             cacheType:RIURLCacheNoCache
+                                                             cacheTime:RIURLCacheDefaultTime
+                                                    userAgentInjection:[RIApi getCountryUserAgentInjection]
+                                                          successBlock:^(RIApiResponse apiResponse, NSDictionary *jsonObject) {
+                                                              
+                                                              NSDictionary *metadata = VALID_NOTEMPTY_VALUE([jsonObject objectForKey:@"metadata"], NSDictionary);
+                                                              
+                                                              successBlock([RIExternalCategory parseExternalCategory:metadata andPersist:YES]);
+                                                              
+                                                          } failureBlock:^(RIApiResponse apiResponse, NSDictionary *errorJsonObject, NSError *errorObject) {
+                                                              if(NOTEMPTY(errorJsonObject))
+                                                              {
+                                                                  failureBlock(apiResponse, [RIError getErrorMessages:errorJsonObject]);
+                                                              } else if(NOTEMPTY(errorObject))
+                                                              {
+                                                                  NSArray *errorArray = [NSArray arrayWithObject:[errorObject localizedDescription]];
+                                                                  failureBlock(apiResponse, errorArray);
+                                                              } else
+                                                              {
+                                                                  failureBlock(apiResponse, nil);
+                                                              }
+                                                          }];
+}
+
++ (RIExternalCategory *)parseExternalCategory:(NSDictionary *)externalCategoryJSON andPersist:(BOOL)persist
+{
+    RIExternalCategory* newExternalCategory = nil;
+    if (persist) {
+        newExternalCategory = (RIExternalCategory*)[[RIDataBaseWrapper sharedInstance] managedObjectOfType:NSStringFromClass([RIExternalCategory class])];
+    }else{
+        newExternalCategory = (RIExternalCategory*)[[RIDataBaseWrapper sharedInstance] temporaryManagedObjectOfType:NSStringFromClass([RIExternalCategory class])];
+    }
+    
+    [newExternalCategory setLabel:VALID_NOTEMPTY_VALUE([externalCategoryJSON objectForKey:@"label"], NSString)];
+    [newExternalCategory setPosition:VALID_NOTEMPTY_VALUE([externalCategoryJSON objectForKey:@"position"], NSNumber)];
+    if (VALID_NOTEMPTY([externalCategoryJSON objectForKey:@"external_links"], NSArray))
+    {
+        [newExternalCategory setLevel:@0];
+        for (NSDictionary *externalLinkJSON in [externalCategoryJSON objectForKey:@"external_links"]) {
+            RIExternalCategory *child = [RIExternalCategory parseExternalCategory:externalLinkJSON andPersist:YES];
+            [child setParent:newExternalCategory];
+        }
+    }else{
+        [newExternalCategory setLevel:@1];
+        NSString *targetString = VALID_NOTEMPTY_VALUE([externalCategoryJSON objectForKey:@"external_link_ios"], NSString);
+        if (VALID_NOTEMPTY_VALUE(targetString, NSString)) {
+            [newExternalCategory setTargetString:[RITarget getTargetString:EXTERNAL_LINK node:targetString]];
+        }
+        [newExternalCategory setImageUrl:VALID_NOTEMPTY_VALUE([externalCategoryJSON objectForKey:@"image"], NSString)];
+    }
+    
+    if (persist) {
+        [[RIDataBaseWrapper sharedInstance] saveContext];
+    }
+    
+    return newExternalCategory;
+}
+
+@end
