6597bbeba458a204fe208edb5bc301e73006dbce
Telmo Pinto <telmopinto@telmopintos-MacBook-Air.local>
2016-05-03 15:47:03 +0100

    Fixed seller delivery view rotations and rtl

--- Jumia/View/JASellerDeliveryView.m
+++ Jumia/View/JASellerDeliveryView.m
@@ -12,6 +12,7 @@
 
 @interface JASellerDeliveryView ()
 
+@property (nonatomic, strong) UIView* contentBackgroundView;
 @property (nonatomic, strong) JAProductInfoBaseLine* headerLine;
 @property (strong, nonatomic) NSMutableArray *quantity;
 @property (strong, nonatomic) NSMutableArray *prodName;
@@ -24,7 +25,12 @@
 
 @implementation JASellerDeliveryView
 
--(void) setupWithSellerDelivery:(RISellerDelivery*)sellerDelivery index:(NSInteger)index ofMax:(NSInteger)max width:(CGFloat)width{
+-(void) setupWithSellerDelivery:(RISellerDelivery*)sellerDelivery index:(NSInteger)index ofMax:(NSInteger)max width:(CGFloat)width
+{
+    if (!self.contentBackgroundView) {
+        self.contentBackgroundView = [UIView new];
+        [self addSubview:self.contentBackgroundView];
+    }
     
     self.backgroundColor = JAWhiteColor;
     
@@ -37,7 +43,7 @@
     
     self.headerLine = [[JAProductInfoHeaderLine alloc] initWithFrame:CGRectMake(0.0f, currentY, self.frame.size.width, kProductInfoHeaderLineHeight)];
     [self.headerLine setTitle:[NSString stringWithFormat:STRING_SHIPMENT_OF,(long)index,(long)max]];
-    [self addSubview:self.headerLine];
+    [self.contentBackgroundView addSubview:self.headerLine];
     
     currentY += self.headerLine.frame.size.height;
     
@@ -53,7 +59,7 @@
         [name setTextColor:JABlackColor];
         [name sizeToFit];
         [name setWidth:width-name.frame.origin.x*2];
-        [self addSubview:name];
+        [self.contentBackgroundView addSubview:name];
         [self.prodName addObject:name];
         
         //qty
@@ -64,14 +70,14 @@
         [qty setFont:JACaptionFont];
         [qty setTextColor:JABlack800Color];
         [qty sizeToFit];
-        [self addSubview:qty];
+        [self.contentBackgroundView addSubview:qty];
         [self.quantity addObject:qty];
         
         //separator
         currentY += qty.frame.size.height + 12.0f;
         UIView* separator = [[UIView alloc] init];
         separator.backgroundColor = JABlack400Color;
-        [self addSubview:separator];
+        [self.contentBackgroundView addSubview:separator];
         [self.separators addObject:separator];
         separator.frame = CGRectMake(startingX,
                                      currentY,
@@ -89,7 +95,7 @@
     [self.name setFont:JABodyFont];
     [self.name setTextColor:JABlackColor];
     [self.name sizeToFit];
-    [self addSubview:self.name];
+    [self.contentBackgroundView addSubview:self.name];
     currentY += self.name.frame.size.height;
     
     //shipping local
@@ -99,7 +105,7 @@
     [self.delivery setFont:JABodyFont];
     [self.delivery setTextColor:JABlackColor];
     [self.delivery sizeToFit];
-    [self addSubview:self.delivery];
+    [self.contentBackgroundView addSubview:self.delivery];
     
     currentY += self.delivery.frame.size.height;
     
@@ -117,41 +123,52 @@
         [self.shipping setNumberOfLines:0];
         [self.shipping sizeToFit];
         
-        [self addSubview:self.shipping];
+        [self.contentBackgroundView addSubview:self.shipping];
         currentY += self.shipping.frame.size.height;
     }
 
-    [self setFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, currentY+12.0f)];
+    self.contentBackgroundView.frame = self.bounds;
+    self.contentBackgroundView.width = width;
+    self.contentBackgroundView.height = currentY+12.0f;
     
     if (RI_IS_RTL) {
-        [self.headerLine flipAllSubviews];
+        [self.contentBackgroundView flipAllSubviews];
     }
 }
 
 -(void)updateWidth:(CGFloat)width
 {
-    [self setFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y, width, self.frame.size.height)];
+    self.contentBackgroundView.frame = self.bounds;
+    self.contentBackgroundView.width = width;
 
     [self.headerLine setFrame:CGRectMake(self.headerLine.frame.origin.x, self.headerLine.frame.origin.y, width, self.headerLine.frame.size.height)];
     
+    CGFloat startingX = 16.0f;
+    
     for (UILabel* qty in self.quantity) {
-        [qty setFrame:CGRectMake(qty.frame.origin.x, qty.frame.origin.y, width, qty.frame.size.height)];
+        [qty setFrame:CGRectMake(startingX, qty.frame.origin.y, width, qty.frame.size.height)];
+        [qty setTextAlignment:NSTextAlignmentLeft];
     }
     
     for (UILabel* prod in self.prodName) {
-        [prod setFrame:CGRectMake(prod.frame.origin.x, prod.frame.origin.y, width, prod.frame.size.height)];
+        [prod setFrame:CGRectMake(startingX, prod.frame.origin.y, width, prod.frame.size.height)];
+        [prod setTextAlignment:NSTextAlignmentLeft];
     }
     
     for (UIView* separator in self.separators) {
-        [separator setFrame:CGRectMake(separator.frame.origin.x, separator.frame.origin.y, width, separator.frame.size.height)];
+        [separator setFrame:CGRectMake(startingX, separator.frame.origin.y, width, separator.frame.size.height)];
     }
     
-    [self.name setFrame:CGRectMake(self.name.frame.origin.x, self.name.frame.origin.y, width, self.name.frame.size.height)];
-    [self.delivery setFrame:CGRectMake(self.delivery.frame.origin.x, self.delivery.frame.origin.y, width, self.delivery.frame.size.height)];
-    [self.shipping setFrame:CGRectMake(self.shipping.frame.origin.x, self.shipping.frame.origin.y, width, self.shipping.frame.size.height)];
+    [self.headerLine setFrame:CGRectMake(0.0f, self.headerLine.frame.origin.y, width, self.headerLine.frame.size.height)];
+    [self.name setFrame:CGRectMake(startingX, self.name.frame.origin.y, width, self.name.frame.size.height)];
+    [self.name setTextAlignment:NSTextAlignmentLeft];
+    [self.delivery setFrame:CGRectMake(startingX, self.delivery.frame.origin.y, width, self.delivery.frame.size.height)];
+    [self.delivery setTextAlignment:NSTextAlignmentLeft];
+    [self.shipping setFrame:CGRectMake(startingX, self.shipping.frame.origin.y, width, self.shipping.frame.size.height)];
+    [self.shipping setTextAlignment:NSTextAlignmentLeft];
     
     if (RI_IS_RTL) {
-        [self.headerLine flipAllSubviews];
+        [self.contentBackgroundView flipAllSubviews];
     }
 }
 
