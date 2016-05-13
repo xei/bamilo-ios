104fa8633cb503216bf6438e3e0af21860c033c8
Jose Mota <jose.mota@africainternetgroup.com>
2016-05-13 12:11:34 +0100

    removing debug nslogs

--- Jumia/Controllers/OnlineReturns/JAORCallToReturnViewController.m
+++ Jumia/Controllers/OnlineReturns/JAORCallToReturnViewController.m
@@ -138,18 +138,15 @@
         if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tel:123456"]])
         {
             [self.callButton setEnabled:YES];
-            NSLog(@"Device can make call or send message");
         }
         else
         {
             [self.callButton setEnabled:NO];
-            NSLog(@"Device can not make call or send message");
         }
     }
     else
     {
         [self.callButton setEnabled:NO];
-        NSLog(@"Device can not make call or send message");
     }
 }
 
